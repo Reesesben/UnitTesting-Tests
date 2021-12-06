c772a192e853268fc7e850e0ab6773bf7aedd8b2
Ben Erekson <42879076+Reeseben@users.noreply.github.com>
2021-12-05 21:57:48 -0700

    Add Project files and structure

---
+++ UnitTesting/UnitTesting/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTesting
+//
+//  Created by Ben Erekson on 12/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
